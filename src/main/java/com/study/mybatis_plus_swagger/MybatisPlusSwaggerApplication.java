9bad7f80fcdc0fadba8929c5f081c5a0dde08fc3
lqx <982521616@qq.com>
2020-11-09 18:44:34 +0800

    创建项目

---
+++ src/main/java/com/study/mybatis_plus_swagger/MybatisPlusSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.study.mybatis_plus_swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusSwaggerApplication.class, args);
+    }
+
+}
